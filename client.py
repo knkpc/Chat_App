7521f89be7023a5d9bdd351f26ceb47c498b72bd
RD <a.rakeshdatta@gmail.com>
2025-01-29 00:34:12 -0600

    client program

--- client.py
+++ client.py
@@ -1,17 +1,34 @@
 import socket
 import pickle
+import sys
 import time
 import yaml
 
+from concheck import configChk
+
 class usr_Reg:
     userName : str = None
-    userPhone: str ="0000000000"
+    userPhone: str =None
     userUUID: str =None
 
-    def __init__(self, name:str,phone:str):
+    def newusr(self, name:str,phone:str):
+        print(f"newusr: name: {name} , phone: {phone}")
         self.userName=name
-        self.userPhone=phone
+        if self.checkPhone(phone):
+            
+            self.userPhone=phone
+        else:
+            print(f"newusr: phone check failure")
+            return False
         self.userUUID=self.getUUID(name,phone)
+        if self.userUUID != None:
+            configChk.createusr(dict(name=self.userName, phone=self.userPhone,UUID=self.userUUID))
+        else:
+            print(f"newusr: UUID Failed")
+            return False
+        return True
+            
+    
 
     def getUUID(self,name:str, phone:str):
         uuid=None
@@ -32,9 +49,98 @@ class usr_Reg:
                 time.sleep(1)
 
         return uuid
+    
+    def checkPhone(self,phone:str):
+        """Check if the phone number is valid or not
+         - should have 10 digits
+         - should contain all digits
+        """
+        if len(phone) ==10:
+            for d in phone:
+                if not d.isdigit():
+                    return False
+        else:
+            return False
+        return True
 
 
-     
+class clientmain:
+
+    def main():
+        pass
+
+    def loader():
+        # for i in range(100):
+        #     print("=",end="")
+        configChk.checkusr()
+        for i in range(10):
+            sys.stdout.write("/ loading user details.." if i % 2 == 0 else "\\")  # Print / or \
+            sys.stdout.flush()  # Ensure it is written immediately
+            time.sleep(0.2)  # Wait for a brief moment before switching
+            sys.stdout.write("\r")  # Return cursor to the start of the line
+
+        # Add a final print to make sure the cursor is on a new line after finishing
+        print()
+
+    def userdetails():
+        return configChk.readusr()
+    
+    def validUSR():
+        """This checks for user file and validates user data in the file
+            return:
+            - 1 user details are valid
+            - -1 user file doesn't exist
+            - -2 username doesn't exist
+            - -3 phone doesn't exist
+            - -4 UUID doesn't exist 
+
+        
+        """
+        if configChk.checkusr():
+            data=configChk.readusr()
+            count =-1
+            for key,value in data.items():
+                #print(f"{key}:{value}")
+                count-=1
+                if value is None:
+                    return count
+        else:
+            return -1
+        return 1
+                    
+
+    def loadusr():
+        if clientmain.validUSR() ==1:
+            
+            return(clientmain.userdetails())
+        else:
+            print("New User")
+            return(clientmain.Register())
+        
+    def Register():
+        choice = input("do you want to Register? yes no")
+        if choice=="yes" or "y" or "Yes":
+            username = input("your name: ")
+            phone = input("your phone:")
+
+            while not usr_Reg().checkPhone(phone):
+                phone = input("Enter Valid phone")
+
+            nusr= usr_Reg().newusr(username,phone)
+            if nusr:
+                return "Registered"
+            else:
+                return "Registration Failed, please try again later"
+        else:
+            print("You need to register to continue..")
+
+
+
+
+
+
+clientmain.loader()
+print(clientmain.loadusr())
 
 
     
--- concheck.py
+++ concheck.py
@@ -7,6 +7,7 @@ USER_FILE = Path("usr.yaml")
 class configChk:
 
     def checkconfig(c_file=CONFIG_FILE):
+        """Returns True value if the 'config.yaml' exists """
         if c_file.exists():
             return True
         else:
@@ -16,6 +17,7 @@ class configChk:
         if c_file.exists():
             return True
         else:
+            configChk.createusr()
             return False
         
     def createconfig():
@@ -29,22 +31,47 @@ class configChk:
             data = yaml.safe_load(file)
         return data
 
-    def createusr():
-        data=dict(name=None, phone=None,UUID=None)
+    def createusr(data=dict(name=None, phone=None,UUID=None)):
+        
         with open(USER_FILE,'w') as file:
             yaml.dump(data,file)
+        return True
 
     def readusr():
+        """This function returns the data from user file 'usr.yaml' 
+        - received data is a dict 
+        ex:data=dict(name=None, phone=None,UUID=None)
+        """
+        if not configChk.checkusr():
+            return "Error : user file doesn't Exist"
         data:dict
         with open(USER_FILE,'r') as file:
             data = yaml.safe_load(file)
+        #print(data)
         return data
     
-print(configChk.checkconfig())
-print(configChk.checkusr())
+    def setUUID(UUID:str, replace:bool =False):
+        """This function is used to stored the UUID received from the server in the usr.yaml file, 
+        to replace the existing UUID set replace value to true"""
+        if configChk.checkusr():
+            data = configChk.readusr()
+            if data["UUID"]!=None or replace:
+                data["UUID"]=UUID
+                with open(USER_FILE,'w') as file:
+                    yaml.dump(data,file)
+                    return "UUID Stored successfully"
+            else:
+                return "Error: UUID already exists want to replace set replace = True "
+            
+        else:
+            return "Error: Usr.yaml file doesn't exist"
 
-print(configChk.createusr())
-print(configChk.readusr())      
 
-print(configChk.createconfig())
-print(configChk.readconfig())  
+# print(configChk.checkconfig())
+# print(configChk.checkusr())
+
+# print(configChk.createusr())
+# print(configChk.readusr())      
+
+# print(configChk.createconfig())
+# print(configChk.readconfig())  
